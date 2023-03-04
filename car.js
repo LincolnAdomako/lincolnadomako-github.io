5ec7ae25efc3980ea03e2fa366b479a01f433864
LincolnAdomako <124861009+LincolnAdomako@users.noreply.github.com>
2023-03-04 22:24:11 +0400

    Added interactive for car file

---
+++ car.js
@@ -0,0 +1,6 @@
+let carBody=document.getElementById("Layer_2")
+
+carBody.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "This car belongs to Lincoln Adomako!";
+    document.getElementById("Layer_test").innerHTML = "";
+})
